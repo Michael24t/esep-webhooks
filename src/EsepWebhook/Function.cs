78a465951d762efffbcfc5fb0e729da8ac1fca19
Michael24t <101540156+Michael24t@users.noreply.github.com>
2025-04-12 22:21:17 -0400

    Update Function.cs

--- src/EsepWebhook/Function.cs
+++ src/EsepWebhook/Function.cs
@@ -1,21 +1,47 @@
+using System.Text;
 using Amazon.Lambda.Core;
+using Newtonsoft.Json;
+using System.Net.Http;
 
-// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
-
 namespace EsepWebhook;
-
 public class Function
 {
-    
-    /// <summary>
-    /// A simple function that takes a string and does a ToUpper
-    /// </summary>
-    /// <param name="input">The event for the Lambda function handler to process.</param>
-    /// <param name="context">The ILambdaContext that provides methods for logging and describing the Lambda environment.</param>
-    /// <returns></returns>
-    public string FunctionHandler(string input, ILambdaContext context)
+    private static readonly HttpClient client = new HttpClient();
+    public string FunctionHandler(object input, ILambdaContext context)
     {
-        return input.ToUpper();
+        context.Logger.LogInformation("Webhook triggered");
+        context.Logger.LogInformation($"Input received: {input}");
+
+        try
+        {
+            dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
+
+            string issueUrl = json.issue.html_url;
+            context.Logger.LogInformation($"Parsed issue URL: {issueUrl}");
+
+            string slackMessage = $"{{\"text\": \"issue: {issueUrl}\"}}";
+            var webhookUrl = Environment.GetEnvironmentVariable("SLACK_URL");
+            if (string.IsNullOrEmpty(webhookUrl))
+            {
+                context.Logger.LogError("SLACK_URL enviro not set");
+                return "Slack URL not configured.";
+            }
+
+           var request = new HttpRequestMessage(HttpMethod.Post, webhookUrl)
+            {
+                Content = new StringContent(slackMessage, Encoding.UTF8, "application/json")
+            };
+
+         var response = client.Send(request);
+            context.Logger.LogInformation($"Slack response: {response.StatusCode}");
+
+            return "Webhook processed successfully.";
+        }
+        catch (Exception ex)
+        {
+            context.Logger.LogError($"Exception: {ex.Message}");
+            return $"Error: {ex.Message}";
+        }
     }
 }
